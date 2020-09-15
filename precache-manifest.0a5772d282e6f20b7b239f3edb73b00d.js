ec300945fe80157dafc5bd352da387ecd8a458fb
Shreya Joshi <shreyajoshi1905@gmail.com>
2020-09-15 18:15:55 +0530

    Updates

---
+++ precache-manifest.0a5772d282e6f20b7b239f3edb73b00d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128e6599dc879a022751ddcc1ad35451",
+    "url": "/cookingwebsi/index.html"
+  },
+  {
+    "revision": "4d568b64a1cefb8f8949",
+    "url": "/cookingwebsi/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "6b97336c5ee1cbaf89db",
+    "url": "/cookingwebsi/static/css/main.86db2d03.chunk.css"
+  },
+  {
+    "revision": "4d568b64a1cefb8f8949",
+    "url": "/cookingwebsi/static/js/2.5635b21f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cookingwebsi/static/js/2.5635b21f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b97336c5ee1cbaf89db",
+    "url": "/cookingwebsi/static/js/main.66f73e88.chunk.js"
+  },
+  {
+    "revision": "7b84f38e17c7a8db4938",
+    "url": "/cookingwebsi/static/js/runtime-main.52c70a31.js"
+  },
+  {
+    "revision": "72e332d95660649f737e78bf0e97f92a",
+    "url": "/cookingwebsi/static/media/img2.72e332d9.jpg"
+  },
+  {
+    "revision": "0b91b109f0374154222729cb4519f6f8",
+    "url": "/cookingwebsi/static/media/img3.0b91b109.jpg"
+  },
+  {
+    "revision": "713fe31a39961b341d2d5e8e262650d9",
+    "url": "/cookingwebsi/static/media/img4.713fe31a.jpg"
+  },
+  {
+    "revision": "1e719de6b30cca907057e57d2b32bb57",
+    "url": "/cookingwebsi/static/media/img7.1e719de6.jpg"
+  },
+  {
+    "revision": "0650fd345b1b039e72bc40d8e01313a5",
+    "url": "/cookingwebsi/static/media/img8.0650fd34.jpg"
+  }
+]);
